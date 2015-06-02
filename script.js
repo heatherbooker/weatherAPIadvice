6fc74d272f5f2728680d1ee5b20dedb5ab109d5e
Heather Booker <heather.booker@hotmail.com>
2015-06-02 16:27:15 -0700

    further structure, call API

--- script.js
+++ script.js
@@ -8,4 +8,20 @@ $(document).ready(function() {
         console.log(dayToForecast);
     });
 
+    //reference API
+    $.ajax({
+        beforeSend: function(request) {
+            request.setRequestHeader("X-Mashape-Key", "QZEnKyYBkDmshpGpiYZ7HEq66Iuwp14dcvojsnHzeVRVBn06Ij");
+            request.setRequestHeader("Accept", "application/json");
+        },
+        type: "GET",
+        url: "https://george-vustrey-weather.p.mashape.com/api.php?location=Vancouver",
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(err) {
+            console.log("ERROR" + err);
+        }
+    });
+
 });
