0135d768b40e66885157d463f5a555b3bd2c908b
Heather Booker <heather.booker@hotmail.com>
2015-06-05 14:46:54 -0700

    api error

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
 $(document).ready(function() {
 
-    //whenever a different day is selected:
-    $('#dayMenu').change(function() {
+    $('button').click(function() {
+
         // check day selected
-        var dayToForecast = $('#dayMenu').val();
-        // report weather forcast
-        console.log(dayToForecast);
+        var day = $('#dayMenu').val();
+        var dayToForecast = "";
+
+        function shortenDayName(day) {
+            if (day === "Sunday") {
+                dayToForecast = "Sun";
+            } else if (day === "Wednesday") {
+                dayToForecast = "Wed";
+            }
+        }
+        shortenDayName(day);
+
+        //get location to check forecast for
+        var location = $('#locus').val();
+        location = location.charAt(0).toUpperCase() + location.substring(1);
+
+
+
+        //reference API
+        $.ajax({
+            beforeSend: function(request) {
+                request.setRequestHeader("x-api-key", "47801ec2546320154de9cf8c92afef22");
+            },
+            type: "GET",
+            url: "api.openweathermap.org/data/2.5/forecast?q=London",
+            success: function(data) {
+                console.log(data);
+            },
+            error: function(err) {
+                console.log("ERROR" + err.stringify);
+            }
+        });
+
+        function getDayObject(data, dayToForecast) {
+            for (var i = 0; i < 7; i++) {
+                if (dayToForecast === data[i].day_of_week) {
+                    console.log('getting there');
+                }
+            }
+        }
+
     });
-
-    //get location to check forecast for
-    var location = $('#locus').val();
-
-    //reference API
-    // $.ajax({
-    //     beforeSend: function(request) {
-    //         request.setRequestHeader("X-Mashape-Key", "QZEnKyYBkDmshpGpiYZ7HEq66Iuwp14dcvojsnHzeVRVBn06Ij");
-    //         request.setRequestHeader("Accept", "application/json");
-    //     },
-    //     type: "GET",
-    //     url: "https://george-vustrey-weather.p.mashape.com/api.php?location=Vancouver",
-    //     success: function(data) {
-    //         console.log(data);
-    //     },
-    //     error: function(err) {
-    //         console.log("ERROR" + err);
-    //     }
-    // });
-
 });
